845c3653471bda6c027a667f6dd7d0829a696cc9
danyel-dev <dannykill4221x@gmail.com>
2022-12-22 23:45:51 -0300

    question of game of old created

---
+++ Exercises/jogoDaVelha/jogoDaVelha/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace jogoDaVelha
+{
+    class Program
+    {
+        static void Main()
+        {
+            int jogada;
+            char[] vet = new char[9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine($"  { vet[0]} | {vet[1]}  | {vet[2]} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($"  {vet[3]} | {vet[4]}  | {vet[5]} ");
+                Console.WriteLine("---+---+---");
+                Console.WriteLine($"  {vet[6]} | {vet[7]}  | {vet[8]} ");
+                Console.WriteLine();
+
+                Console.Write("Escolha uma posição para jogar: ");
+
+                jogada = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    vet[jogada] = 'X';
+                }
+                else
+                {
+                    vet[jogada] = 'O';
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
